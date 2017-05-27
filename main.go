7d3250552936de7ff3070b7348c297e3973d7cd5
David Gage <whytheplatypus@gmail.com>
2017-05-27 18:48:19 -0400

    Initial commit
    Just a quick program to read github issues
    and transform them into a document

---
+++ main.go
@@ -0,0 +1,90 @@
+// # RCA: A tool to help us deal with rca's in github
+//
+// There are two problems to be solved.
+// It's difficult and annoying to move an issue into
+// a PR.
+// It's hard to search the resulting files in the repo
+//
+// To help with these `rca` will attempt to transform
+// an issue into a md doc that can be submitted in a PR
+//
+// It will also attempt to make that markdown document
+// a bit easier to deal with by adding front matter
+// containing the labels that had been applied to the issue
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/github"
+)
+
+const docTmp = `
+{{.Issue.GetBody}}
+{{range .Comments}}
+{{.GetBody}}
+{{end}}
+{{range .Labels}}
+{{.GetName}}
+{{end}}
+`
+
+func main() {
+	org := os.Args[1]
+	repo := os.Args[2]
+	iNum, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		panic(err)
+	}
+	// Get a github token from a .ghtoken file
+	ghToken, err := ioutil.ReadFile(fmt.Sprintf("%s%s", os.Getenv("HOME"), "/.ghtoken"))
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: strings.TrimSpace(string(ghToken))},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	issue, _, err := client.Issues.Get(ctx, org, repo, iNum)
+	if err != nil {
+		panic(err)
+	}
+
+	comments, _, err := client.Issues.ListComments(ctx, org, repo, iNum, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	labels, _, err := client.Issues.ListLabelsByIssue(ctx, org, repo, iNum, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.Must(template.New("rca-pr").Parse(docTmp))
+	data := struct {
+		Issue    *github.Issue
+		Labels   []*github.Label
+		Comments []*github.IssueComment
+	}{
+		issue,
+		labels,
+		comments,
+	}
+	if err := t.Execute(os.Stdout, data); err != nil {
+		panic(err)
+	}
+}
